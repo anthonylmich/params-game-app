09849cbb412d16e107e7d3d41c52d4543cf597e5
Anthony <tonymich2012@gmail.com>
2022-01-12 13:29:49 -0800

    adds segment parameters

--- app/controllers/params_game_controller.rb
+++ app/controllers/params_game_controller.rb
@@ -24,5 +24,32 @@ class ParamsGameController < ApplicationController
     end
     render json: {message: "your guess is #{number}, " + message}
   end
+  
+  def sum
+    number1 = params[:wildcard1]
+    number2 = params[:wildcard2]
+    sum = number1.to_i + number2.to_i
+    render json: {message: "The sum of your two numbers is #{sum}"}
+  end
+  def num_game
+    random_number = 36
+    number = params[:wildcard1]
+    number = number.to_i
+    if number == random_number
+      message = "you guessed right!"
+    elsif number > random_number
+      message = "you guessed too high"
+    elsif number < random_number
+      message = "you guessed too low"
+    end
+    render json: {message: "your guess is #{number}, " + message}
+  end
 
+
+  def name_game2
+    word = params[:wildcard]
+    if word.first == "a"
+      render json: {message: "Your name starts with the first letter of the alphabet!"}
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   # root "articles#index"
   get "/name"=>"params_game#name"
   get "/number_game"=>"params_game#number"
-  get "/name_game"=>"params_game#name_game"
+  get "/name_game"=>"params_game#name_game
+  "
+  get "name_game2/:wildcard" => "params_game#name_game2"
+  get "sum_of/:wildcard1/:wildcard2" => "params_game#sum"
+  get "num_game/:wildcard1" => "params_game#num_game"
+
 end
